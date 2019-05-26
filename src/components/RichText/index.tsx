869ccf64b1094eb4c66e35b7fc0f1f4588fd827f
ColMugX <colmugx@qq.com>
2019-05-26 23:12:29 +0800

    update: wxParse

--- src/components/RichText/index.tsx
+++ src/components/RichText/index.tsx
@@ -1,32 +1,33 @@
-import Taro, { PureComponent } from '@tarojs/taro'
-import { View } from '@tarojs/components'
-import WxParse from '../wxParse/wxParse'
-import './index.scss'
+import Taro, { PureComponent } from '@tarojs/taro';
+import { View } from '@tarojs/components';
+import WxParse from '../wxParse/wxParse';
+import './index.scss';
+import '../wxParse/wxParse.wxss';
 
 interface IProps {
-  nodes: string
-  className: string
+  nodes: string;
+  className: string;
 }
 
 class RichText extends PureComponent<IProps> {
   constructor(props: IProps) {
-    super(props)
+    super(props);
   }
 
   componentDidMount() {
-    const { nodes } = this.props
-    WxParse.wxParse('article', 'html', nodes, this.$scope, 5)
+    const { nodes } = this.props;
+    WxParse.wxParse('article', 'html', nodes, this.$scope, 5);
   }
 
   render() {
-    const { className } = this.props
+    const { className } = this.props;
     return (
       <View className={[className, 'body'].join(' ')}>
         <import src="../wxParse/wxParse.wxml" />
         <template is="wxParse" data="{{wxParseData:article.nodes}}" />
       </View>
-    )
+    );
   }
 }
 
-export default RichText
+export default RichText;
--- src/components/wxParse/htmlparser.js
+++ src/components/wxParse/htmlparser.js
@@ -1,13 +1,13 @@
 /**
- *
+ * 
  * htmlParser改造自: https://github.com/blowsie/Pure-JavaScript-HTML5-Parser
- *
+ * 
  * author: Di (微信小程序开发工程师)
  * organization: WeAppDev(微信小程序开发论坛)(http://weappdev.com)
  *               垂直微信小程序开发交流社区
- *
+ * 
  * github地址: https://github.com/icindy/wxParse
- *
+ * 
  * for: 微信小程序富文本解析
  * detail : http://weappdev.com/t/wxparse-alpha0-1-html-markdown/184
  */
